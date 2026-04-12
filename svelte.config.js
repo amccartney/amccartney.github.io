59b63f70431371934efb7002ae498d16c0841d8c
Allison McCartney <allison.n.mccartney@gmail.com>
2026-04-11 17:51:12 -0700

    Refactor legacy project data import to use file system methods

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
-import legacyProjects from './src/lib/data/legacy-projects.json' assert { type: 'json' };
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const legacyProjects = JSON.parse(
+	readFileSync(join(__dirname, 'src/lib/data/legacy-projects.json'), 'utf8')
+);
 
 const projectSlugs = legacyProjects.map((project) => `/projects/${project.slug}`);
 
